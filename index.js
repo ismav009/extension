3c094750d6b138dff3fb3ed1723baf7dac6b8a91
ismav009 <vamsi66666v@gmail.com>
2025-12-18 11:38:50 +0000

    commented a function

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let myLeads = [];
 let oldLeads = [];
 const save_el = document.getElementById("save-btn");
@@ -9,75 +8,65 @@ let delete_el = document.getElementById("delete-btn");
 let tab_el = document.getElementById("tab-btn");
 let old_el = document.getElementById("old-btn");
 
-
-
-const leadsfromLocalstorage = JSON.parse(localStorage.getItem("myLeads"))// get leads from local storage
+const leadsfromLocalstorage = JSON.parse(localStorage.getItem("myLeads")); // get leads from local storage
 // console.log(leadsfromLocalstorage);
-
 
 // const tabs = [
 //   {url: "https://www.linkedin.com/in/vamsi-krishna"}
 // ]
 
-tab_el.addEventListener("click", function() {
-   chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-      myLeads.push(tabs[0].id);
-      localStorage.setItem("myLeads", JSON.stringify(myLeads));
-      render(myLeads);
-   });
+tab_el.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].id);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
 });
 
-save_el.addEventListener("click", function() {
-   myLeads.push(input_el.value);
-   // ul_el.innerHTML += "<li>" + input_el.value + "</li>";
+save_el.addEventListener("click", function () {
+  myLeads.push(input_el.value);
+  // ul_el.innerHTML += "<li>" + input_el.value + "</li>";
 
-   localStorage.setItem("myLeads", JSON.stringify(myLeads));
-   console.log( localStorage.getItem("myLeads"));
-   render(myLeads);
-   
-   input_el.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  console.log(localStorage.getItem("myLeads"));
+  render(myLeads);
 
-})
+  input_el.value = "";
+});
 
 if (leadsfromLocalstorage) {
-   myLeads = leadsfromLocalstorage;
-   render(myLeads);
+  myLeads = leadsfromLocalstorage;
+  render(myLeads);
 }
 
 function render(leads) {
-   let listItems = "";
-   for (let i = 0; i < leads.length; i++) {
-
-
-   listItems += `
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
    <li>
      <a target="_blank" href="${leads[i]}">
       ${leads[i]}
      </a>
     </li>;
-   `
+   `;
   }
-ul_el.innerHTML = listItems;
+  ul_el.innerHTML = listItems;
 }
 
-
-delete_el.addEventListener("dblclick", function() {
-   oldLeads = myLeads;
-   localStorage.clear();
-   myLeads = [];
-   render(myLeads);
+delete_el.addEventListener("dblclick", function () {
+  oldLeads = myLeads;
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
 });
 
-old_el.addEventListener("click", function() {
-   render(oldLeads);
+old_el.addEventListener("click", function () {
+  render(oldLeads);
 });
 
-
-
-   // const li = document.createElement("li");
-   // li.textContent = myLeads[i];
-   // ul_el.appendChild(li);
-
+// const li = document.createElement("li");
+// li.textContent = myLeads[i];
+// ul_el.appendChild(li);
 
 /*
  function renderLead() {
@@ -87,24 +76,21 @@ old_el.addEventListener("click", function() {
 
 */
 
-
 // falsy values: 0, "", null(how you as a developer ignalizes emptiness), undefined(how javascript signalizes emptiness), NaN, false
 
+// const dict = [
+//   {"largest countries": "[china, india, usa]"}
+// ]
 
-const dict = [
-  {"largest countries": "[china, india, usa]"}
-]
+// function generateSentence(desc, arr) {
+//    let sentence = `The ${arr.length} ${desc} are `;
+//    for (let i = 0; i < arr.length; i++) {
+//       sentence += arr[i];
+//       if (i < arr.length - 1) {
+//          sentence += ", ";
+//       }
+//    }
+//    return sentence;
+// }
 
-function generateSentence(desc, arr) {
-   let sentence = `The ${arr.length} ${desc} are `;
-   for (let i = 0; i < arr.length; i++) {
-      sentence += arr[i];
-      if (i < arr.length - 1) {
-         sentence += ", ";
-      }
-   }
-   return sentence;
-}
-
-console.log(generateSentence(dict[0]));
-
+// console.log(generateSentence(dict[0]));
